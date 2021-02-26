a08b036e038dfdc6b4849fc2c47805eda9a24e3d
Valtteri Ranta <waltherbeach@gmail.com>
2021-02-26 14:47:35 +0200

    parse CSV file to a Map with Dates as keys

--- controllers/local.js
+++ controllers/local.js
@@ -1,4 +1,7 @@
-const { parseCSV } = require('../utils/csv')
+const { 
+  parseCSVtoArray,
+  parseCSVtoMap,
+} = require('../utils/csv')
 const { readCSV } = require('../services/local');
 
 // @desc returns all data from a stored csv file as JSON
@@ -7,8 +10,9 @@ const { readCSV } = require('../services/local');
 exports.getAllData = async (req, res, next) => {
   try {
     const csv = await readCSV(req.params.fileName);
-    const parsedCSV = parseCSV(csv);
-    res.status(200).json(parsedCSV);
+    const parsedCSVMap = parseCSVtoMap(csv);
+    const parsedCSVMapAsObj = Object.fromEntries(parsedCSVMap);
+    res.status(200).json(parsedCSVMapAsObj);
   } catch (err) {
     next(err);
   }
--- utils/csv.js
+++ utils/csv.js
@@ -2,7 +2,7 @@
  * Parses CSV from String to an array of arrays
  * @param {String} csv Content of a CSV file as a String
  */
-exports.parseCSV = function(csv) {
+exports.parseCSVtoArray = function(csv) {
   //Split csv String into an array of lines
   let lines = csv.split(/\r\n|\n/);
 
@@ -15,11 +15,32 @@ exports.parseCSV = function(csv) {
   lines.shift();
 
   // Split rest of the csv file lines into arrays and push them into result
-  let lineArray = [];
   lines.forEach(line => {
-    let lineArray = line.split(',');
+    lineArray = line.split(',');
     result.push(lineArray);
-  })
+  });
 
+  return result;
+}
+
+exports.parseCSVtoMap = function(csv) {
+  let lines = csv.split(/\r\n|\n/);
+  let result = new Map();
+
+  // Add data to Map with date as key and rest of the data as properties of an object as value
+  lines.forEach(line => {
+    lineArray = line.split(',');
+    // Assuming the MM/DD/YYYY syntax from Nasdaq's csv files, omit header and empty rows
+    if (lineArray[0].length === 10) {
+      let date = new Date(lineArray[0]);
+      result.set(date, {
+        close: lineArray[1],
+        volume: lineArray[2],
+        open: lineArray[3],
+        high: lineArray[4],
+        low: lineArray[5]
+      });
+    }
+  });
   return result;
 }
